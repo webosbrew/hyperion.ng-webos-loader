d6b41bd07e793728c0c73b6b75d36b1671a454b5
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-06-10 02:50:23 +0200

    feat: Spawn process via posix_spawn

--- service/src/service.h
+++ service/src/service.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <unistd.h>
 #include <glib.h>
+#include <pthread.h>
 
 #define SERVICE_NAME "org.webosbrew.hyperion.ng.loader.service"
 #define HYPERION_PATH "/media/developer/apps/usr/palm/services/org.webosbrew.hyperion.ng.loader.service/hyperion"
 
 typedef struct {
-    bool running;
     char *hyperiond_version;
+    pid_t daemon_pid;
+    pthread_t execution_thread;
 } service_t;
 
 int service_start(service_t* service);
