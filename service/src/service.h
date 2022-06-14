bdd3505267477da51c677d5b8e97fa99c6e14db7
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-06-14 06:26:38 +0200

    native service: Split into a few source files

--- service/src/service.h
+++ service/src/service.h
@@ -3,12 +3,16 @@
 #include <unistd.h>
 #include <glib.h>
 #include <pthread.h>
+#include <luna-service2/lunaservice.h>
 
 #define LOG_NAME "hyperion.ng-loader"
 #define SERVICE_NAME "org.webosbrew.hyperion.ng.loader.service"
 #define DAEMON_PATH "/media/developer/apps/usr/palm/services/org.webosbrew.hyperion.ng.loader.service/hyperion"
 #define DAEMON_EXECUTABLE "hyperiond"
 #define DAEMON_NAME "Hyperion.NG"
+
+// Global from main.c
+extern GMainLoop *gmainLoop;
 
 typedef struct {
     char *daemon_version;
@@ -18,3 +22,4 @@ typedef struct {
 
 int service_start(service_t* service);
 int service_stop(service_t* service);
+bool service_init(LSHandle *handle, service_t *service, LSError *lserror);
